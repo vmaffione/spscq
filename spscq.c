cdbba90ec1da78ebfe080a032b0be94c32cdd052
Vincenzo Maffione <v.maffione@gmail.com>
2017-12-26 12:52:26 +0100

    msq: read side functions

--- spscq.c
+++ spscq.c
@@ -24,6 +24,12 @@ struct mbuf {
     char buf[MBUF_LEN_MAX];
 };
 
+struct mbuf *
+mbuf_alloc()
+{
+    return szalloc(sizeof(struct mbuf));
+}
+
 /* Multi-section queue, based on the Lamport classic queue.
  * All indices are free running. */
 struct msq {
@@ -71,17 +77,12 @@ msq_create(int qlen, int rbatch)
 }
 
 static unsigned int
-msq_rspace(struct msq *mq)
-{
-    return mq->write - mq->read_priv;
-}
-
-static unsigned int
 msq_wspace(struct msq *mq)
 {
     return (mq->read - 1 - mq->write_priv) & mq->qmask;
 }
 
+/* No boundary checks, to be called after msq_wspace(). */
 static void
 msq_write_local(struct msq *mq, struct mbuf *m)
 {
@@ -109,19 +110,56 @@ msq_write(struct msq *mq, struct mbuf *m)
     return 0;
 }
 
+static unsigned int
+msq_rspace(struct msq *mq)
+{
+    return mq->write - mq->read_priv;
+}
+
+/* No boundary checks, to be called after msq_rspace(). */
+static struct mbuf *
+msq_read_local(struct msq *mq)
+{
+    struct mbuf *m = mq->q[mq->read_priv];
+    mq->read_priv++;
+    return m;
+}
+
+static void
+msq_read_publish(struct msq *mq)
+{
+    mq->read = mq->read_priv;
+}
+
+static struct mbuf *
+msq_read(struct msq *mq)
+{
+    struct mbuf *m;
+
+    if (mq->read_priv == mq->write) {
+        return NULL; /* no space */
+    }
+    m = msq_read_local(mq);
+    msq_read_publish(mq);
+
+    return m;
+}
+
 int
 main(int argc, char **argv)
 {
-    struct mbuf m;
-
-    memset(&m, 0, sizeof(m));
-
     {
         struct msq *mq = msq_create(128, 4);
+        struct mbuf *m = mbuf_alloc();
+
         printf("wspace %u rspace %u\n", msq_wspace(mq), msq_rspace(mq));
-        msq_write(mq, &m);
+        m->len = 18;
+        msq_write(mq, m);
+        printf("wspace %u rspace %u\n", msq_wspace(mq), msq_rspace(mq));
+        m = msq_read(mq);
         printf("wspace %u rspace %u\n", msq_wspace(mq), msq_rspace(mq));
         free(mq);
+        free(m);
     }
 
     return 0;
