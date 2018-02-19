0d1a8594c39d350fd389bb77b474c186e9ffe139
Vincenzo Maffione <v.maffione@gmail.com>
2018-02-19 17:39:15 +0100

    run-tests: parallel sequencing

--- run-tests.py
+++ run-tests.py
@@ -13,13 +13,14 @@ epilog = "2018 Vincenzo Maffione <v.maffione@gmail.com>"
 
 argparser = argparse.ArgumentParser(description = description,
                                     epilog = epilog)
-argparser.add_argument('--spin-min', help = "Minimum emulated load (spin)",
+argparser.add_argument('--spin-min', help = "Minimum emulated load (cycles)",
                        type = int, default = 5)
-argparser.add_argument('--delta-step', help = "Step to use to increase delta",
+argparser.add_argument('--spin-step', help = "Step to use to increase spin",
                        type = int, default = 5)
-argparser.add_argument('--delta-max', help = "Max delta between P and C spins",
+argparser.add_argument('--spin-max', help = "Maximum emulated load (cycles)",
                        type = int, default = 50)
-argparser.add_argument('--duration', help = "Duration of a test run in seconds",
+argparser.add_argument('-D', '--duration',
+                       help = "Duration of a test run in seconds",
                        type = int, default = 10)
 argparser.add_argument('--trials', help = "How many test runs per point",
                        type = int, default = 10)
@@ -32,22 +33,44 @@ argparser.add_argument('-l', '--queue-length', help = "Queue length",
                        type = int, default = 256)
 argparser.add_argument('-b', '--batch-size', help = "Batch size",
                        type = int, default = 32)
+argparser.add_argument('-S', '--sequencing', type = str, default = 'parallel',
+                       choices = ['parallel', 'crossed'],
+                       help = "How P and C emulated load vary")
 
 args = argparser.parse_args()
 
 queues = ['lq', 'llq', 'blq', 'ffq', 'iffq', 'biffq']
 results = {}
 
+if args.spin_min > args.spin_max:
+    print("Error: spin_min > spin_max")
+    quit(1)
+
+if args.spin_min < 0:
+    print("Error: spin_min < 1")
+    quit(1)
+
+if args.spin_step < 1:
+    print("Error: spin_step < 1")
+    quit(1)
+
+if args.trials < 1:
+    print("Error: trias < 1")
+    quit(1)
+
 try:
     spin_p = args.spin_min
-    spin_c = args.spin_min + args.delta_max
-    while spin_p <= args.spin_min + args.delta_max:
+    if args.sequencing == 'parallel':
+        spin_c = spin_p
+    else:
+        spin_c = args.spin_max
+
+    while spin_p <= args.spin_max:
         results[(spin_p, spin_c)] = {}
         for queue in queues:
-            cmd = './spscq -D %d -P %d -C %d -t %s -l %d '\
-                  '-L %d -b %d' % (args.duration, spin_p, spin_c, queue,
-                                args.queue_length, args.batch_size,
-                                args.batch_size)
+            cmd = './spscq -D %d -l %d -L %d -b %d -P %d -C %d -t %s'\
+                     % (args.duration, args.queue_length, args.batch_size,
+                        args.batch_size, spin_p, spin_c, queue)
             if args.indirect:
                 cmd += ' -M'
             if args.exp_type == 'latency':
@@ -68,8 +91,12 @@ try:
                         mpps_values.append(mpps)
                         print("Got %f Mpps" % mpps)
             results[(spin_p, spin_c)][queue] = mpps_values
-        spin_p += args.delta_step
-        spin_c -= args.delta_step
+
+        spin_p += args.spin_step
+        if args.sequencing == 'parallel':
+            spin_c = spin_p
+        else:
+            spin_c -= args.spin_step
 except KeyboardInterrupt:
     print("Interrupted. Bye.")
     quit(1)
@@ -80,7 +107,10 @@ for (p, c) in results:
     row = [p, c, p-c]
     for queue in queues:
         avg = statistics.mean(results[(p, c)][queue])
-        std = statistics.stdev(results[(p, c)][queue])
+        if args.trials > 1:
+            std = statistics.stdev(results[(p, c)][queue])
+        else:
+            std = 0.0
         row.append(avg)
         row.append(std)
     fmt = '%8s ' * 3
