cef5205530672a7e894f8ec1ca654c30e3526fb7
Vincenzo Maffione <v.maffione@gmail.com>
2018-02-23 17:32:22 +0100

    evolve tests

--- exp-array.sh
+++ exp-array.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 
-SPIN_STEP=3
-SPIN_MAX=72
+SPIN_STEP_THR=3
+SPIN_MAX_THR=72
+SPIN_STEP_LAT=100
+SPIN_MAX_LAT=0
 
-./run-tests.py --spin-min 0 --spin-max $SPIN_MAX --spin-step $SPIN_STEP -S parallel | tee exp/001
-./run-tests.py --spin-min 0 --spin-max $SPIN_MAX --spin-step $SPIN_STEP -S crossed | tee exp/002
-./run-tests.py --spin-min 0 --spin-max $SPIN_MAX --spin-step $SPIN_STEP -S ptriangle | tee exp/003
-./run-tests.py --spin-min 0 --spin-max $SPIN_MAX --spin-step $SPIN_STEP -S ctriangle | tee exp/004
+./run-tests.py --spin-min 0 --spin-max $SPIN_MAX_THR --spin-step $SPIN_STEP_THR -S parallel | tee exp/001
+./run-tests.py --spin-min 0 --spin-max $SPIN_MAX_THR --spin-step $SPIN_STEP_THR -S crossed | tee exp/002
+./run-tests.py --spin-min 0 --spin-max $SPIN_MAX_THR --spin-step $SPIN_STEP_THR -S ptriangle | tee exp/003
+./run-tests.py --spin-min 0 --spin-max $SPIN_MAX_THR --spin-step $SPIN_STEP_THR -S ctriangle | tee exp/004
 
-./run-tests.py -M --spin-min 0 --spin-max $SPIN_MAX --spin-step $SPIN_STEP -S parallel | tee exp/005
-./run-tests.py -M --spin-min 0 --spin-max $SPIN_MAX --spin-step $SPIN_STEP -S crossed | tee exp/006
-./run-tests.py -M --spin-min 0 --spin-max $SPIN_MAX --spin-step $SPIN_STEP -S ptriangle | tee exp/007
-./run-tests.py -M --spin-min 0 --spin-max $SPIN_MAX --spin-step $SPIN_STEP -S ctriangle | tee exp/008
+./run-tests.py -M --spin-min 0 --spin-max $SPIN_MAX_THR --spin-step $SPIN_STEP_THR -S parallel | tee exp/005
+./run-tests.py -M --spin-min 0 --spin-max $SPIN_MAX_THR --spin-step $SPIN_STEP_THR -S crossed | tee exp/006
+./run-tests.py -M --spin-min 0 --spin-max $SPIN_MAX_THR --spin-step $SPIN_STEP_THR -S ptriangle | tee exp/007
+./run-tests.py -M --spin-min 0 --spin-max $SPIN_MAX_THR --spin-step $SPIN_STEP_THR -S ctriangle | tee exp/008
 
-./run-tests.py --exp-type latency --spin-min 0 --spin-max $SPIN_MAX --spin-step $SPIN_STEP -S parallel | tee exp/009
-./run-tests.py --exp-type latency --spin-min 0 --spin-max $SPIN_MAX --spin-step $SPIN_STEP -S crossed | tee exp/010
-./run-tests.py --exp-type latency --spin-min 0 --spin-max $SPIN_MAX --spin-step $SPIN_STEP -S ptriangle | tee exp/011
-./run-tests.py --exp-type latency --spin-min 0 --spin-max $SPIN_MAX --spin-step $SPIN_STEP -S ctriangle | tee exp/012
+./run-tests.py --exp-type latency --spin-min 0 --spin-max $SPIN_MAX_LAT --spin-step $SPIN_STEP_LAT -S parallel | tee exp/009
+./run-tests.py --exp-type latency --spin-min 0 --spin-max $SPIN_MAX_LAT --spin-step $SPIN_STEP_LAT -S crossed | tee exp/010
+./run-tests.py --exp-type latency --spin-min 0 --spin-max $SPIN_MAX_LAT --spin-step $SPIN_STEP_LAT -S ptriangle | tee exp/011
+./run-tests.py --exp-type latency --spin-min 0 --spin-max $SPIN_MAX_LAT --spin-step $SPIN_STEP_LAT -S ctriangle | tee exp/012
+
+./run-tests.py -M --exp-type latency --spin-min 0 --spin-max $SPIN_MAX_LAT --spin-step $SPIN_STEP_LAT -S parallel | tee exp/013
+./run-tests.py -M --exp-type latency --spin-min 0 --spin-max $SPIN_MAX_LAT --spin-step $SPIN_STEP_LAT -S crossed | tee exp/014
+./run-tests.py -M --exp-type latency --spin-min 0 --spin-max $SPIN_MAX_LAT --spin-step $SPIN_STEP_LAT -S ptriangle | tee exp/015
+./run-tests.py -M --exp-type latency --spin-min 0 --spin-max $SPIN_MAX_LAT --spin-step $SPIN_STEP_LAT -S ctriangle | tee exp/016
