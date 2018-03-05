dfe025a18ed1529c4158edfa2cbfa51d30eee1a8
Vincenzo Maffione <v.maffione@gmail.com>
2018-03-05 15:42:49 +0100

    remove old experiments

--- exp-array.sh
+++ exp-array.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-EXPDIR=exp2
+EXPDIR=exp1
 NUM_POINTS_THR=30
-NUM_POINTS_LAT=1
-TRIALS=5
+TRIALS=10
+SPIN_MIN=5
 
-./run-tests.py --trials $TRIALS --spin-min 0 --num-points $NUM_POINTS_THR -S parallel | tee $EXPDIR/001
-./run-tests.py --trials $TRIALS --spin-min 0 --num-points $NUM_POINTS_THR -S ptriangle | tee $EXPDIR/002
-./run-tests.py --trials $TRIALS --spin-min 0 --num-points $NUM_POINTS_THR -S ctriangle | tee $EXPDIR/003
+./run-tests.py --trials $TRIALS --spin-min 0 --num-points 1 -S parallel | tee $EXPDIR/001
+./run-tests.py --trials $TRIALS --spin-min $SPIN_MIN --num-points $NUM_POINTS_THR -S ptriangle | tee $EXPDIR/002
+./run-tests.py --trials $TRIALS --spin-min $SPIN_MIN --num-points $NUM_POINTS_THR -S ctriangle | tee $EXPDIR/003
 
-./run-tests.py --trials $TRIALS -M --spin-min 0 --num-points $NUM_POINTS_THR -S parallel | tee $EXPDIR/004
-./run-tests.py --trials $TRIALS -M --spin-min 0 --num-points $NUM_POINTS_THR -S ptriangle | tee $EXPDIR/005
-./run-tests.py --trials $TRIALS -M --spin-min 0 --num-points $NUM_POINTS_THR -S ctriangle | tee $EXPDIR/006
+./run-tests.py --trials $TRIALS -M --spin-min 0 --num-points 1 -S parallel | tee $EXPDIR/004
+./run-tests.py --trials $TRIALS -M --spin-min $SPIN_MIN --num-points $NUM_POINTS_THR -S ptriangle | tee $EXPDIR/005
+./run-tests.py --trials $TRIALS -M --spin-min $SPIN_MIN --num-points $NUM_POINTS_THR -S ctriangle | tee $EXPDIR/006
 
-./run-tests.py --trials $TRIALS --exp-type latency --spin-min 0 --num-points $NUM_POINTS_LAT -S parallel | tee $EXPDIR/007
-./run-tests.py --trials $TRIALS -M --exp-type latency --spin-min 0 --num-points $NUM_POINTS_LAT -S parallel | tee $EXPDIR/008
+./run-tests.py --trials $TRIALS --exp-type latency --spin-min 0 --num-points 1 -S parallel | tee $EXPDIR/007
+./run-tests.py --trials $TRIALS -M --exp-type latency --spin-min 0 --num-points 1 -S parallel | tee $EXPDIR/008
