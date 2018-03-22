1a2ee27cb62902aa4496fa6e800694dade21fb70
Vincenzo Maffione <v.maffione@gmail.com>
2018-03-22 17:22:42 +0100

    improve get perf results

--- get-perf-top-results.sh
+++ get-perf-top-results.sh
@@ -10,13 +10,10 @@ DUR=${2:-2}
 OUTF=${3:-cms.out}
 
 TMPF=$(mktemp)
-sudo perf stat -C $CORE -e cycles,instructions,L1-dcache-loads,L1-dcache-load-misses,L1-dcache-stores,L1-dcache-store-misses sleep $DUR > $TMPF 2>&1
+sudo perf stat -C $CORE -e instructions,L1-dcache-load-misses,L1-dcache-store-misses sleep $DUR > $TMPF 2>&1
 #cat $TMPF
-LD=$(grep "L1-dcache-loads" $TMPF | awk '{print $1}')
 LDMISS=$(grep "L1-dcache-load-misses" $TMPF | awk '{print $1}')
-ST=$(grep "L1-dcache-stores" $TMPF | awk '{print $1}')
 STMISS=$(grep "L1-dcache-store-misses" $TMPF | awk '{print $1}')
-CYCLE=$(grep "cycles" $TMPF | awk '{print $1}')
 INSN=$(grep "instructions" $TMPF | awk '{print $1}')
 SECS=$(grep "seconds time elapsed" $TMPF | awk '{print $1}')
 #rm $TMPF
