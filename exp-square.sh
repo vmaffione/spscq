f3c3cfce62fc7f4e7bceff0cc862befdf774e8bd
Vincenzo Maffione <v.maffione@gmail.com>
2018-03-23 19:27:18 +0100

    add verbose mode

--- exp-square.sh
+++ exp-square.sh
@@ -8,11 +8,11 @@ DURATION=5
 STEP=10
 #DRY_RUN=--dry-run
 
-./run-tests.py -w $DRY_RUN --duration $DURATION --max-trials $TRIALS --step $STEP \
+./run-tests.py -vw $DRY_RUN --duration $DURATION --max-trials $TRIALS --step $STEP \
                 --spin-min $SPMIN --num-points $N -S square | tee $EXPDIR/001
-./run-tests.py -w $DRY_RUN --duration $DURATION --max-trials $TRIALS --step $STEP \
+./run-tests.py -vw $DRY_RUN --duration $DURATION --max-trials $TRIALS --step $STEP \
                 --spin-min $SPMIN --num-points $N -S square -M | tee $EXPDIR/002
-./run-tests.py -w $DRY_RUN --duration $DURATION --max-trials $TRIALS  --step $STEP \
+./run-tests.py -vw $DRY_RUN --duration $DURATION --max-trials $TRIALS  --step $STEP \
                 --spin-min $SPMIN --num-points $N -S square --Bp 1 --Bc 1 | tee $EXPDIR/003
-./run-tests.py -w $DRY_RUN --duration $DURATION --max-trials $TRIALS --step $STEP \
+./run-tests.py -vw $DRY_RUN --duration $DURATION --max-trials $TRIALS --step $STEP \
                 --spin-min $SPMIN --num-points $N -S square -M --Bp 1 --Bc 1 | tee $EXPDIR/004
