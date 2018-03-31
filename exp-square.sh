5a7a103b111924b75fb8ad7ad28269de9aedc036
Vincenzo Maffione <v.maffione@gmail.com>
2018-03-31 12:55:56 +0200

    merge exp scripts

--- exp-square.sh
+++ exp-square.sh
@@ -6,13 +6,18 @@ TRIALS=3
 N=10
 DURATION=5
 STEP=10
-#DRY_RUN=--dry-run
+#MORE="--dry-run"
 
-./run-tests.py -vw $DRY_RUN --duration $DURATION --max-trials $TRIALS --step $STEP \
+./run-tests.py -vw $MORE --duration $DURATION --max-trials $TRIALS --step $STEP \
                 --spin-min $SPMIN --num-points $N -S square | tee $EXPDIR/001
-./run-tests.py -vw $DRY_RUN --duration $DURATION --max-trials $TRIALS --step $STEP \
+./run-tests.py -vw $MORE --duration $DURATION --max-trials $TRIALS --step $STEP \
                 --spin-min $SPMIN --num-points $N -S square -M | tee $EXPDIR/002
-./run-tests.py -vw $DRY_RUN --duration $DURATION --max-trials $TRIALS  --step $STEP \
+./run-tests.py -vw $MORE --duration $DURATION --max-trials $TRIALS  --step $STEP \
                 --spin-min $SPMIN --num-points $N -S square --Bp 1 --Bc 1 | tee $EXPDIR/003
-./run-tests.py -vw $DRY_RUN --duration $DURATION --max-trials $TRIALS --step $STEP \
+./run-tests.py -vw $MORE --duration $DURATION --max-trials $TRIALS --step $STEP \
                 --spin-min $SPMIN --num-points $N -S square -M --Bp 1 --Bc 1 | tee $EXPDIR/004
+
+./run-tests.py -vw $MORE --duration $DURATION --max-trials $TRIALS \
+                --exp-type latency --spin-min 0 --num-points 1 | tee $EXPDIR/005
+./run-tests.py -vw $MORE --duration $DURATION --max-trials $TRIALS -M \
+                --exp-type latency --spin-min 0 --num-points 1 | tee $EXPDIR/006
