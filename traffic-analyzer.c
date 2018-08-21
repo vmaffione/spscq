3e0cec12c63bba8b0e8bcc38dd61926d24ad2d76
Vincenzo Maffione <v.maffione@gmail.com>
2018-08-21 20:35:07 +0200

    ta: thread infrastructure

--- traffic-analyzer.c
+++ traffic-analyzer.c
@@ -2,10 +2,50 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <pthread.h>
+#include <errno.h>
+
+#include "mlib.h"
+
+struct worker {
+    pthread_t th;
+};
 
 struct traffic_analyzer {
     unsigned int num_analyzers;
+    pthread_t lb_th;
+    struct worker *workers;
 };
+
+/* Alloc zeroed cacheline-aligned memory, aborting on failure. */
+static void *
+szalloc(size_t size)
+{
+    void *p = NULL;
+    int ret = posix_memalign(&p, ALIGN_SIZE, size);
+    if (ret) {
+        printf("allocation failure: %s\n", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+    memset(p, 0, size);
+    return p;
+}
+
+static void *
+lb(void *opaque)
+{
+    struct traffic_analyzer *ta = opaque;
+    (void)ta;
+    return NULL;
+}
+
+static void *
+analyze(void *opaque)
+{
+    struct worker *w = opaque;
+    (void)w;
+    return NULL;
+}
 
 static void
 usage(const char *progname)
@@ -23,6 +63,7 @@ main(int argc, char **argv)
     struct traffic_analyzer _ta;
     struct traffic_analyzer *ta = &_ta;
     int opt;
+    int i;
 
     memset(ta, 0, sizeof(*ta));
     ta->num_analyzers = 2;
@@ -46,6 +87,38 @@ main(int argc, char **argv)
             break;
         }
     }
+
+    /* Setup phase. */
+    ta->workers = szalloc(ta->num_analyzers * sizeof(ta->workers[0]));
+
+    if (pthread_create(&ta->lb_th, NULL, lb, ta)) {
+        printf("pthread_create(lb) failed\n");
+        exit(EXIT_FAILURE);
+    }
+
+    for (i = 0; i < ta->num_analyzers; i++) {
+        struct worker *w = ta->workers + i;
+        if (pthread_create(&w->th, NULL, analyze, w)) {
+            printf("pthread_create(worker) failed\n");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    /* Teardown phase. */
+    for (i = 0; i < ta->num_analyzers; i++) {
+        struct worker *w = ta->workers + i;
+        if (pthread_join(w->th, NULL)) {
+            printf("pthread_join(worker) failed\n");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    if (pthread_join(ta->lb_th, NULL)) {
+        printf("pthread_join(lb) failed\n");
+        exit(EXIT_FAILURE);
+    }
+
+    free(ta->workers);
 
     return 0;
 }
